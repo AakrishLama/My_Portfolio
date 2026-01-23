072d5e2b5dfa90d14e9342e87af97f462580d61d
Aakrish Lama <al226na@student.lnu.se>
2026-01-23 02:35:15 +0100

    base setup of the application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>My Portfolio</h1>
+      <p>Project setup complete. Ready to build!</p>
+    </div>
+  )
+}
+
+export default App
